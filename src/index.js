11119cfc1ff0d4cf401d1aaf3ee8e35498fc4670
Yohem <vaxelaire.yohem@gmail.com>
2023-07-26 17:15:39 +0200

    correction desc

--- src/index.js
+++ src/index.js
@@ -15,11 +15,11 @@ formation.render(
 )
 
 pro.render(
-  <ProjetPro name="Location de Maison" imgsLink="./assets/location/0.png,./assets/location/1.png,./assets/location/2.png" ></ProjetPro>
+  <ProjetPro name="Location de Maison" imgsLink="./assets/location/0.png,./assets/location/1.png,./assets/location/2.png" >Dans le cadre de ce projet, j'ai développé une plateforme en ligne conviviale dédiée à la location de logements. En utilisant PHP, JavaScript et MySQL, j'ai créé un site web interactif qui présente des appartements avec leurs caractéristiques, des descriptions engageantes et des images captivantes. Les visiteurs peuvent naviguer aisément à travers les annonces, vérifier la disponibilité des logements et obtenir des informations complètes sur les tarifs. De plus, j'ai intégré un panneau d'administration sécurisé pour permettre une gestion facile des logements et des réservations, sans nécessiter de compétences techniques.</ProjetPro>
 )
 
 perso.render(
-  <div><ProjetPerso name="Journal Du Dév" imgsLink="./assets/JDD/0.png,./assets/JDD/1.png">Ici est la descripption</ProjetPerso><ProjetPerso name="Histoire de la programmation" imgsLink="./assets/histoirProg/0.png,./assets/histoirProg/1.png">Site Web avec mini-jeux sur l'histoire de la programmation</ProjetPerso></div>
+  <div><ProjetPerso name="Journal Du Dév" imgsLink="./assets/JDD/0.png,./assets/JDD/1.png">J'ai réalisé un site web dynamique axé sur la création d'une communauté passionnée de programmation. Le site offre des cours interactifs, des forums de discussion et des interfaces utilisateur conviviales. J'ai utilisé Node JS et MySQL pour développer ce projet, en veillant à ce qu'il soit sécurisé et facile à gérer grâce à un système d'administration dédié.</ProjetPerso><ProjetPerso name="Histoire de la programmation" imgsLink="./assets/histoirProg/0.png,./assets/histoirProg/1.png">Site Web avec mini-jeux sur l'histoire de la programmation réaliser durant mes années de lycée.</ProjetPerso></div>
 )
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
