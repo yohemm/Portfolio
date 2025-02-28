1960b773fa8a419602cb44acc1e7b1f81c92a8e8
yohem vaxelaire <vaxelaire.yohem@gmail.com>
2025-02-28 07:10:14 +0000

    test: fix test test file extension

--- tests/unit/toolsAndServices.test.tsx
+++ tests/unit/toolsAndServices.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { render } from '@testing-library/react';
-
+import { describe, it, expect } from 'vitest';
 import { ToolsAndServices } from '~/components/ToolsAndServices';
 
 describe('<ToolsAndServices>', () => {
@@ -13,140 +13,140 @@ describe('<ToolsAndServices>', () => {
     const services = render(<ToolsAndServices services={['Grafana']} tools={[]} />).container;
 
     expect(toolsAndServices).toMatchInlineSnapshot(`
-          <div>
+      <div>
+        <div
+          id="container-subtitle-async tools-and-services-container"
+        >
+          <div
+            class="technos-listing-container"
+          >
             <div
-              id="container-subtitle-async tools-and-services-container"
+              class="subtitle-container"
+            >
+              <h3
+                class="subtitle"
+              >
+                 
+                Les outils que j'ai pu utilisé :
+              </h3>
+            </div>
+            <div
+              class="technos-listing-title"
             >
               <div
-                class="technos-listing-container"
+                class="technos-container "
               >
-                <div
-                  class="subtitle-container"
-                >
-                  <h3
-                    class="subtitle"
-                  >
-                     
-                    Les outils que j'ai pu utilisé :
-                  </h3>
-                </div>
-                <div
-                  class="technos-listing-title"
-                >
-                  <div
-                    class="technos-container "
-                  >
-                    <i
-                      class="devicon-git-plain colored"
-                    />
-                    <p>
-                      Git
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div
-                class="technos-listing-container"
-              >
-                <div
-                  class="subtitle-container"
-                >
-                  <h3
-                    class="subtitle"
-                  >
-                     
-                    Les services que j'ai pu utilisé :
-                  </h3>
-                </div>
-                <div
-                  class="technos-listing-title"
-                >
-                  <div
-                    class="technos-container "
-                  >
-                    <i
-                      class="devicon-grafana-plain colored"
-                    />
-                    <p>
-                      Grafana
-                    </p>
-                  </div>
-                </div>
+                <i
+                  class="devicon-git-plain colored"
+                />
+                <p>
+                  Git
+                </p>
               </div>
             </div>
           </div>
-        `);
+          <div
+            class="technos-listing-container"
+          >
+            <div
+              class="subtitle-container"
+            >
+              <h3
+                class="subtitle"
+              >
+                 
+                Les services que j'ai pu utilisé :
+              </h3>
+            </div>
+            <div
+              class="technos-listing-title"
+            >
+              <div
+                class="technos-container "
+              >
+                <i
+                  class="devicon-grafana-plain colored"
+                />
+                <p>
+                  Grafana
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    `);
     expect(tools).toMatchInlineSnapshot(`
-          <div>
+      <div>
+        <div
+          id="container-subtitle-async tools-and-services-container"
+        >
+          <div
+            class="technos-listing-container"
+          >
             <div
-              id="container-subtitle-async tools-and-services-container"
+              class="subtitle-container"
+            >
+              <h3
+                class="subtitle"
+              >
+                 
+                Les outils que j'ai pu utilisé :
+              </h3>
+            </div>
+            <div
+              class="technos-listing-title"
             >
               <div
-                class="technos-listing-container"
+                class="technos-container "
               >
-                <div
-                  class="subtitle-container"
-                >
-                  <h3
-                    class="subtitle"
-                  >
-                     
-                    Les outils que j'ai pu utilisé :
-                  </h3>
-                </div>
-                <div
-                  class="technos-listing-title"
-                >
-                  <div
-                    class="technos-container "
-                  >
-                    <i
-                      class="devicon-git-plain colored"
-                    />
-                    <p>
-                      Git
-                    </p>
-                  </div>
-                </div>
+                <i
+                  class="devicon-git-plain colored"
+                />
+                <p>
+                  Git
+                </p>
               </div>
             </div>
           </div>
-        `);
+        </div>
+      </div>
+    `);
     expect(services).toMatchInlineSnapshot(`
-          <div>
+      <div>
+        <div
+          id="container-subtitle-async tools-and-services-container"
+        >
+          <div
+            class="technos-listing-container"
+          >
             <div
-              id="container-subtitle-async tools-and-services-container"
+              class="subtitle-container"
+            >
+              <h3
+                class="subtitle"
+              >
+                 
+                Les services que j'ai pu utilisé :
+              </h3>
+            </div>
+            <div
+              class="technos-listing-title"
             >
               <div
-                class="technos-listing-container"
+                class="technos-container "
               >
-                <div
-                  class="subtitle-container"
-                >
-                  <h3
-                    class="subtitle"
-                  >
-                     
-                    Les services que j'ai pu utilisé :
-                  </h3>
-                </div>
-                <div
-                  class="technos-listing-title"
-                >
-                  <div
-                    class="technos-container "
-                  >
-                    <i
-                      class="devicon-grafana-plain colored"
-                    />
-                    <p>
-                      Grafana
-                    </p>
-                  </div>
-                </div>
+                <i
+                  class="devicon-grafana-plain colored"
+                />
+                <p>
+                  Grafana
+                </p>
               </div>
             </div>
           </div>
-        `);
+        </div>
+      </div>
+    `);
   });
 });
